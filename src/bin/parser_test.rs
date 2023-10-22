af496b4bf6ac097d8690ecc739579c936272ddf3
yong chen <yongchen0823@gmail.com>
2023-10-22 19:00:09 +0800

    add

--- src/bin/parser_test.rs
+++ src/bin/parser_test.rs
@@ -31,7 +31,7 @@ fn main() -> Result<()> {
         tests_failed: Vec::new(),
     };
 
-    let filenames = Some(&["tests1.dat"][..]);
+    let filenames = Some(&["tests2.dat"][..]);
     let fixtures = testing::tree_construction::fixtures(filenames).expect("fixtures");
 
     for fixture_file in fixtures {
@@ -43,7 +43,7 @@ fn main() -> Result<()> {
 
         let mut test_idx = 1;
         for test in fixture_file.tests {
-            // if test_idx == 23 {
+            // if test_idx ==  63 {
             run_tree_test(test_idx, &test, &mut results);
             // }
             test_idx += 1;
--- src/html5/parser.rs
+++ src/html5/parser.rs
@@ -1212,7 +1212,7 @@ impl<'stream> Html5Parser<'stream> {
                         Token::EndTagToken { name, .. } if name == "optgroup" => {
                             if current_node!(self).name == "option"
                                 && self.open_elements.len() > 1
-                                && open_elements_get!(self, self.open_elements.len() - 1).name
+                                && open_elements_get!(self, self.open_elements.len() - 2).name
                                     == "optgroup"
                             {
                                 self.open_elements.pop();
@@ -2245,9 +2245,12 @@ impl<'stream> Html5Parser<'stream> {
                     let node = open_elements_get!(self, idx);
                     let tag = node.name.clone();
 
-                    if ["dd", "dt"].contains(&tag.as_str()) {
+                    if tag == "dd" || tag == "dt" {
                         self.generate_implied_end_tags(Some(tag.as_str()), false);
-                        self.open_elements.pop();
+                        if current_node!(self).name != tag {
+                            self.parse_error("dd tag not at top of stack");
+                        }
+                        self.pop_until(tag.as_str());
                         break;
                     }
 
